c2b27d5a107d90cf27e50023b5b1189361855a10
Camila <cpgiudice@hotmail.com>
2023-04-21 17:48:37 -0300

    Estructura de la clase, con property, Constructores y Métodos por hacer comentada.

--- Cine.cs
+++ Cine.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,58 @@ namespace TP1_GrupoB
 {
     internal class Cine
     {
+
+
+        /*
+       //Property 
+        public List<Usuario> Usuarios = new List<Usuario> { Get; Set; }
+        public List<Funcion> Funciones = new List<Funcion> { Get; Set;};
+        public List<Sala> Salas = new List<Sala> { Get; Set;};
+        public List<Pelicula> peliculas = new List<Pelicula> { Get; Set;};
+        public Usuario UsuarioActual { Get; Set; }
+
+
+
+       //Constructores
+        
+        public Cine(){
+        }
+
+        public Cine(List<Usuario> Usuarios, List<Funcion> Funciones, List<Sala> Salas, List<Pelicula> Peliculas, Usuario UsuarioActual) {
+
+        }      
+
+
+
+       // ABM Entidades()
+       
+       // CargarCredito(int idUsuario, double importe){
+        } 
+        
+       // ComprarEntrada(int idUsuario, int cant){
+        }
+
+       // DevolverEntrada(){
+        }
+
+       // IniciarSesion(){
+        }
+      
+       // CerrarSesion(){
+        }
+
+       // MostrarFunciones(): List<Funcion>
+
+       // MostrarSalas(): List<Sala>
+
+       // MostrarPeliculas(): List<Post>   ¿?Así estaba en el diagrama
+
+       // BuscarFuncion(string Ubicacion, Date Fecha, double Costo, string pelicula): List<Funcion>  
+        
+        
+       */
+
+
     }
+    
 }
