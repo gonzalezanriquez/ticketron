f33b73e3c4ce4705dc8b0e077f1508e348753dc0
gonzalezanriquez <gonzalezanriquez@gmail.com>
2023-04-24 11:05:36 -0300

    Login 11:05

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,14 @@ namespace TP1_GrupoB
         public Form1()
         {
             InitializeComponent();
+            Cine cine = new Cine();
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
